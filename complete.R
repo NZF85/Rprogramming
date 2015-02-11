8b3afc7e4cec62e4694d21bf364f2e34d6d37208
NZF85 <ngzhenfu@gmail.com>
2015-02-11 21:22:32 +0800

    Update complete.R

--- complete.R
+++ complete.R
@@ -14,14 +14,19 @@ complete <- function(directory, id = 1:332) {
   ## where 'id' is the monitor ID number and 'nobs' is the
   ## number of complete cases
   
+  ##all files in the directory
   filenames<-list.files(directory,pattern="*.csv", full.names=TRUE)
+  ## create an empty column
   nobs <- c();
   
   for (index in id) {
+    ## reads the files by the index
     file_data <- read.csv(filenames[index], sep = ",");
+    ## pulls all the rows that are complete, that doesnt have na. complete.cases function does that
     complete_cases <- file_data[complete.cases(file_data),];
+    ##nrow counts the number of rows in the matrix. then we put the latest entry below the previous nobs
     nobs <- c(nobs, nrow(complete_cases));
   }
-  
+  ##id comes in a vector e.g. 1,2,3.. so our nobs corresponds to the id and we bind them together
   return(data.frame(cbind(id, nobs)));
 }
